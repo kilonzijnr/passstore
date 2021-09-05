141ceb6557c405a7b478ee7e665d0a408168e4f7
kilonzijnr <vkilonzi05@gmail.com>
2021-09-05 22:23:55 +0300

    created aa delete function for user credentials and login verification feature

--- credentials.py
+++ credentials.py
@@ -1,12 +1,18 @@
+from _typeshed import Self
+
+
 class User:
 
-   # A Class that generates new user credentials
+    """
+ A Class that generates new user credentials
+    """
 
-    User_lst = []
 
-    def __init__(self, first_name, last_name, password, email):
+User_lst = []
 
-        """
+
+def __init__(self, first_name, last_name, password, email):
+    """
         __init__ method that helps us define properties for our objects.
 
         Args:
@@ -15,3 +21,33 @@ class User:
             password: New user password.
             email : New user email address.
             """
+
+    Self.first_name = first_name
+    Self.last_name = last_name
+    Self.password = password
+    Self.email = email
+
+def save_user(self):
+    """
+    saves users credatials to user_lst
+    """
+    User.user_lst.append(self)
+
+    def remove_user(self):
+
+        """
+        remove_user removes User objects from user_lst
+        """
+        User.user_lst.remove(self)
+
+    @classmethod
+
+    def user_login(cls, user_name, password):
+
+        """
+        user_login checks whether username and password are correct
+        """
+        
+        for user in cls.user_list:
+            if user.user_name == user_name and user.password == password:
+                return True
