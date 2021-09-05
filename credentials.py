46383efe02a0a76e89f5c0281c2d5ca7d3fb86eb
kilonzijnr <vkilonzi05@gmail.com>
2021-09-05 14:21:01 +0300

    added init method to allow me to create new user credentials

--- credentials.py
+++ credentials.py
@@ -1,6 +1,17 @@
 class User:
-    
+
    # A Class that generates new user credentials
-    
+
     User_lst = []
-  
+
+    def __init__(self, first_name, last_name, password, email):
+
+        """
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            first_name: New User first name.
+            last_name : New User last name.
+            password: New user password.
+            email : New user email address.
+            """
