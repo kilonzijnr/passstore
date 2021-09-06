fa9577979b78b22f6390cabf9d0a69a5e5fda2a9
kilonzijnr <vkilonzi05@gmail.com>
2021-09-06 07:58:59 +0300

    used _init_ method to define new user credentials

--- credentials.py
+++ credentials.py
@@ -7,3 +7,12 @@ class credentials:
     """
 
     credentials_list = []
+
+def __init__(self, site_name, user_name, password):
+        """
+        __init__ to define properties of new user Credentials
+        Args:
+            site_name: site to which the credentials belong to
+            user_name: username for website's account user
+            password: password for specific account user
+        """
