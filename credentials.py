87f087aebcee321292e0fce9a291e1c754612b07
kilonzijnr <vkilonzi05@gmail.com>
2021-09-06 08:01:21 +0300

    defined credential holders  using self method

--- credentials.py
+++ credentials.py
@@ -8,11 +8,14 @@ class credentials:
 
     credentials_list = []
 
-def __init__(self, site_name, user_name, password):
+def __init__(self, web_name, user_name, password):
         """
         __init__ to define properties of new user Credentials
         Args:
-            site_name: site to which the credentials belong to
+            web_name: site to which the credentials belong to
             user_name: username for website's account user
             password: password for specific account user
         """
+        self.web_name = web_name
+        self.user_name = user_name
+        self.password = password
